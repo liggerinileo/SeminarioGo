1036ef2060e1b53e880a323ba8ec777fdbb6c5c4
Leandro Liggerini <liggerinileo@gmail.com>
2020-12-02 19:28:54 -0300

    Proyecto Mejorado

--- API-Superheroes/cmd/superheroe/superheroesrv.go
+++ API-Superheroes/cmd/superheroe/superheroesrv.go
@@ -7,6 +7,7 @@ import (
 	"strconv"
 
 	"github.com/gin-gonic/gin"
+	"github.com/jmoiron/sqlx"
 	"github.com/liggerinileo/SeminarioGo/API-Superheroes/internal/config"
 	"github.com/liggerinileo/SeminarioGo/API-Superheroes/internal/database"
 	"github.com/liggerinileo/SeminarioGo/API-Superheroes/internal/service"
@@ -23,9 +24,6 @@ func main() {
 		os.Exit(1)
 	}
 
-	// database.PopulateDatabase(db, "./test/mock.sql")
-	database.PopulateDatabase(db, cfg)
-
 	superheroeService, _ := service.NewSuperheroeService(cfg, db)
 
 	makeEndpoints(superheroeService)
@@ -36,50 +34,69 @@ func makeEndpoints(superheroeService service.SuperheroeService) {
 	router := gin.Default()
 	api := router.Group("/api")
 	v1 := api.Group("/v1")
-	sword := v1.Group("/superheroe")
+	superheroe := v1.Group("/superheroe")
 
-	sword.GET("/", func(c *gin.Context) {
+	superheroe.GET("/", func(c *gin.Context) {
 		c.JSON(200, superheroeService.FindAll())
 	})
 
-	sword.GET("/:id", func(c *gin.Context) {
+	superheroe.GET("/:id", func(c *gin.Context) {
 		id := c.Param("id")
 		parseInt, err := strconv.ParseInt(id, 10, 64)
 		if err != nil {
 			fmt.Println(err.Error())
 			return
 		}
-		c.JSON(200, superheroeService.FindById(int(parseInt)))
+		c.JSON(200, superheroeService.FindByID(int(parseInt)))
 	})
 
-	sword.PUT("/:id", func(c *gin.Context) {
+	superheroe.PUT("/:id", func(c *gin.Context) {
 		id := c.Param("id")
-		sword := new(service.Superheroe)
-		err := c.Bind(superheroe)
+		superheroe := new(service.Superheroe)
 		parseInt, err := strconv.ParseInt(id, 10, 64)
 		if err != nil {
 			fmt.Println(err.Error())
 			return
 		}
-		updateSword, err := superheroeService.UpdateSuperheroe(*superheroe, int(parseInt))
-		c.JSON(200, UpdateSuperheroe)
+		updateSuperheroe, err := superheroeService.UpdateSuperheroe(*superheroe, int(parseInt))
+		c.JSON(200, updateSuperheroe)
 	})
 
-	sword.POST("/", func(c *gin.Context) {
-		sword := new(service.Superheroe)
+	superheroe.POST("/", func(c *gin.Context) {
+		superheroe := new(service.Superheroe)
 		err := c.Bind(superheroe)
 		if err != nil {
 			c.JSON(400, gin.H{"msg": err})
 		}
-		updateSword, err := swordService.AddSuperheroe(*superheroe)
+		updateSuperheroe, err := superheroeService.AddSuperheroe(*superheroe)
 		c.JSON(200, updateSuperheroe)
 	})
 
 	router.Run()
 }
 
+// CreateSchema ...
+func CreateSchema(db *sqlx.DB) error {
+	schema := `CREATE TABLE IF NOT EXISTS superheroe (
+		id integer primary key autoincrement,
+		name varchar,
+		strength integer,
+		speed integer,
+		fightingSkills integer,
+		intelligence integer
+		);`
+
+	// execute a query on the server
+	_, err := db.Exec(schema)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
 func readConfig() *config.Config {
-	configFile := flag.String("config", "./configs/config.yaml", "this is the service config")
+	configFile := flag.String("config", "./config/config.yaml", "this is the service config")
 	flag.Parse()
 
 	cfg, err := config.LoadConfig(*configFile)
