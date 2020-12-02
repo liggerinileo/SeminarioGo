60438b3dd3ac1352bfb7f690ac74f63163f22cbf
Leandro Liggerini <liggerinileo@gmail.com>
2020-12-02 19:29:43 -0300

    Problemas de coneccion solucionados

--- API-Superheroes/internal/database/db.go
+++ API-Superheroes/internal/database/db.go
@@ -2,8 +2,6 @@ package database
 
 import (
 	"errors"
-	"fmt"
-	"io/ioutil"
 
 	"github.com/jmoiron/sqlx"
 	"github.com/liggerinileo/SeminarioGo/API-Superheroes/internal/config"
@@ -27,35 +25,4 @@ func NewDatabase(conf *config.Config) (*sqlx.DB, error) {
 	default:
 		return nil, errors.New("invalid db type")
 	}
-}
-
-// PopulateDatabase ...
-func PopulateDatabase(db *sqlx.DB, filepath string) {
-	file, err := ioutil.ReadFile(filepath)
-	if err != nil {
-		fmt.Println(err.Error())
-	}
-
-	query := string(file)
-	_, err = db.Exec(query)
-	if err != nil {
-		fmt.Println(err.Error())
-	}
-}
-
-// CreateSchema ...
-func CreateSchema(db *sqlx.DB) error {
-	schema := `CREATE TABLE IF NOT EXISTS superheroe (
-		id integer primary key autoincrement,
-		name varchar,
-		speciality varchar,
-		damage varchar);`
-
-	// execute a query on the server
-	_, err := db.Exec(schema)
-	if err != nil {
-		return err
-	}
-
-	return nil
 }
